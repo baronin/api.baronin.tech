763f75c649a558e6f75b434c154b54f5e4225001
baronin <baronin.yaroslav@gmail.com>
2023-06-24 21:45:02 +0300

    fix prettier

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,10 +2,10 @@ const Router = require('express');
 const router = new Router();
 const postController = require('../controller/post.controller');
 
-router.post('/post', postController.createPost)
-router.get('/posts', postController.getPosts)
-router.get('/post/:id', postController.getPost)
-router.put('/post/:id', postController.updatePost)
-router.delete('/post/:id', postController.deletePost)
+router.post('/post', postController.createPost);
+router.get('/posts', postController.getPosts);
+router.get('/post/:id', postController.getPost);
+router.put('/post/:id', postController.updatePost);
+router.delete('/post/:id', postController.deletePost);
 
 module.exports = router;
